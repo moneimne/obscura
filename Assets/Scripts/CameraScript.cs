00a99dd9da60033fab06436c2861c71eccfa113a
Richard Lee <agitosan@gmail.com>
2016-04-02 15:03:29 -0400

    Added player and camera controls

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+    public float angular_speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Camera controls
+        transform.Rotate(transform.right * Input.GetAxis("RightStickVertical") * angular_speed, Space.World);
+        transform.Rotate(Vector3.up * Input.GetAxis("RightStickHorizontal") * angular_speed * 1.3f, Space.World);
+	}
+}
---
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovementScript : MonoBehaviour {
+    private Rigidbody rb;
+    public float Speed;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Movement controls
+        Vector3 movement = new Vector3(Input.GetAxis("LeftStickHorizontal"), 0, Input.GetAxis("LeftStickVertical"));
+        if (movement.sqrMagnitude > 0.5f)
+        {
+            movement.Normalize();
+            rb.MovePosition(transform.position + movement * Time.deltaTime * Speed);
+        }
+	}
+}
