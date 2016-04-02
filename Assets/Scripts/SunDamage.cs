be58547a6c913d53dfce6ce7cc4ae7edf78374da
rahwang <rah1@uchicago.edu>
2016-04-02 17:22:16 -0400

    Adding sun damage script file. Not complete

---
+++ Assets/Scripts/SunDamage.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SunDamage : MonoBehaviour {
+	private Rigidbody rb;
+	private GameObject light;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		light = GameObject.Find ("Directional Light");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!(Physics.Raycast(transform.position, light.transform.forward, 100))) 
+			// Apply damage to player.
+			Debug.Log("OUCH");
+	}
+}
