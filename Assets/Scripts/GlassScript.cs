dd822b54a8212faed0941992c3bbc2d42c320e46
rahwang <rah1@uchicago.edu>
2016-04-03 07:19:44 -0400

    Added glass script

---
+++ Assets/Scripts/GlassScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlassScript : MonoBehaviour {
+	public int number;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 sunDir = GameObject.Find ("Directional Light").transform.rotation.eulerAngles;
+		RaycastHit hit;
+		Physics.Raycast (transform.position, -sunDir, out hit);
+		if (hit.distance > 300.0f) {
+			switch (number) {
+			case 1:
+				// Raise towers + contents!
+				break;
+			}
+		}
+	}
+}
