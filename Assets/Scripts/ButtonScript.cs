a91742100dbc74332437479bdb8a70ea2bc85f31
Richard Lee <agitosan@gmail.com>
2016-04-03 09:12:28 -0400

    Set up buildings and buttons

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -2,37 +2,35 @@
 using System.Collections;
 
 public class ButtonScript : MonoBehaviour {
-    public int number;
     bool inRange;
     bool active;
-    GameObject glass, player;
-    Vector3 buttonTarget, glassTarget, posChange;
+    GameObject player;
+    public GameObject glass, target;
+    Vector3 buttonTarget, posChange;
 
     // Use this for initialization
     void Start() {
         inRange = false;
         active = false;
-        glass = GameObject.Find("Magnifying Glass " + number);
         player = GameObject.Find("Player");
         posChange = new Vector3(0.0f, 0.0f, 0.2f);
         buttonTarget = transform.position - posChange;
-        glassTarget = GameObject.Find("Glass Target " + number).transform.position;
     }
 
     // Update is called once per frame
     void Update() {
         inRange = (Vector3.Distance(transform.position, player.transform.position) < 3.0f);
 
-        if (inRange) {
-            Debug.Log("inRange!!");
-        }
+        //if (inRange) {
+        //    Debug.Log("inRange!!");
+        //}
 
         if (inRange && Input.GetButtonDown("LeftButton") && inRange && !active) {
             active = true;
         }
         if (active) {
             transform.position = Vector3.Lerp(transform.position, buttonTarget, 0.05f);
-            glass.transform.position = Vector3.Lerp(glass.transform.position, glassTarget, 0.1f);
+            glass.transform.position = Vector3.Lerp(glass.transform.position, target.transform.position, 0.02f);
         }
     }
 }
